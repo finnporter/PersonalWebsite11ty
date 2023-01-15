847a5e700dad4b0eb4b144924ba0709f1a31d91a
Finn <iamfinnporter@gmail.com>
2023-01-15 17:31:17 +0100

    adds social icon changes author info

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -2,8 +2,8 @@ module.exports = {
 	siteURL: process.env.URL || 'http://localhost:8080',
 	siteName: "Finn Porter",
 	siteDescription:
-		'A highly configurable 11ty template to quickly spin up a fully functional website with TailwindCSS and Alpine.js baked in.',
-	siteImage: '/assets/images/site/default.png',
+		'Finn Porter is a software developer, non-binary and autistic. Not always in that order.',
+	siteImage: '/assets/images/site/default.png', //TODO replace default img
 	lang: 'en',
 	locale: 'en_us',
 	authorName: 'Finn Porter',
